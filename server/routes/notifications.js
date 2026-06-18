7d498a5801a2c7b7dd4f532a007a2b9c48e14c07
coderaniii <itzanjalianand@gmail.com>
2026-06-18 11:28:35 +0530

    fix: notifications.js authMiddleware import casing

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const protect = require('../middleware/authMiddleWare');
+const protect = require('../middleware/authMiddleware');
 const Notification = require('../models/notification');
 const WorkOrder = require('../models/WorkOrder');
 
